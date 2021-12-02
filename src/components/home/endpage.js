aaa35dc8c5beff4e6f8013bc21d4c692554a362f
MzAllison <isituaho@gmail.com>
2021-12-02 15:45:12 +0100

    we

--- src/components/home/endpage.js
+++ src/components/home/endpage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function EndPage() {
-    const [value, setValue] = useState();
+    const [ setValue] = useState();
 
     const Refresh = () => {
         setValue({});
